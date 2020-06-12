5f08d0ade4f328e0cf15dd2a5fcaf598ddf005df
Pouriya <pouriya.un@gmail.com>
2020-06-12 21:17:03 +0430

    Add project files.

---
+++ leave-management/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,14 @@
+﻿using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface ILeaveAllocationRepository : IRepositoryBase<LeaveAllocation>
+    {
+        bool CheckAllocation(int leavetypeid, string employeeid);
+        ICollection<LeaveAllocation> GetLeaveAllocationsByEmployee(string id);
+    }
+}
---
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,84 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CheckAllocation(int leavetypeid, string employeeid)
+        {
+            var period = DateTime.Now.Year;
+            return FindAll()
+                .Where(q => q.EmployeeId == employeeid && q.LeaveTypeId == leavetypeid && q.Period == period)
+                .Any();
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            var LeaveAllocations = _db.LeaveAllocations
+                .Include(q => q.LeaveType)
+                .Include(q => q.Employee)
+                .ToList();
+            return LeaveAllocations;
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            var LeaveAllocation = _db.LeaveAllocations
+                .Include(q => q.LeaveType)
+                .Include(q => q.Employee)
+                .FirstOrDefault(q => q.Id == id);
+            return LeaveAllocation;
+        }
+
+        public ICollection<LeaveAllocation> GetLeaveAllocationsByEmployee(string id)
+        {
+            var period = DateTime.Now.Year;
+            return FindAll()
+                    .Where(q => q.EmployeeId == id && q.Period == period)
+                    .ToList();
+        }
+
+        public bool isExists(int id)
+        {
+            var exists = _db.LeaveAllocations.Any(q => q.Id == id);
+            return exists;
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Update(entity);
+            return Save();
+        }
+    }
+}
